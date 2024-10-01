e498bacd6eb0e195936b45dc52e00d0dd93761af
Lee Gyu Chan <hoo788@gmail.com>
2024-10-01 15:49:37 +0900

    Initial commit

---
+++ src/main/java/com/dodo/happinessnanumbackend/HappinessNanumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dodo.happinessnanumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappinessNanumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappinessNanumBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dodo/happinessnanumbackend/HappinessNanumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodo.happinessnanumbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappinessNanumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
